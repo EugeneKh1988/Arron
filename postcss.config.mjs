3bde9f4790e2958e64ce3a50c26407f7c4b1fc4b
Evgeniy <khiznyak.1988@gmail.com>
2025-06-10 14:37:06 +0300

    first commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,19 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {},
+    "postcss-preset-mantine": {},
+    "postcss-simple-vars": {
+      variables: {
+        "mantine-breakpoint-xs": "36em",
+        "mantine-breakpoint-sm": "40em",
+        "mantine-breakpoint-md": "48em",
+        "mantine-breakpoint-lg": "64em",
+        "mantine-breakpoint-xl": "80em",
+        "mantine-breakpoint-2xl": "96em",
+        "mantine-breakpoint-3xl": "120em",
+      },
+    },
+  },
 };
 
 export default config;
